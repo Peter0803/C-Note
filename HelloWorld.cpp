61e3d9d351707143f7bd39e2eff8837ddee7b04d
Peter0803 <111713800+Peter0803@users.noreply.github.com>
2022-09-08 11:24:10 +0800

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,12 +4,12 @@ int main(){
     
     //std : stander, cout : character out, std::endl or \n : next line
 
-    // std::cout << "I love pasta." << std::endl;
-    // std::cout << "Hello World!" << std::endl;
-    // std::cout << "I love pasta.\n" + "Hello World!";
-    // std::cout << "I love pasta." + "\nHello World!";
-    // std::cout << "I love pasta." << '\n' << "Hello World!"  << '\n';
-    // std::cout << "I love pasta." << std::endl << "Hello World!"  << std::endl;
+    std::cout << "C++." << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "C++\n" + "Hello World!";
+    std::cout << "C++" + "\nHello World!";
+    std::cout << "C++" << '\n' << "Hello World!"  << '\n';
+    std::cout << "C++" << std::endl << "Hello World!"  << std::endl;
 
     //int number : declare that number stores integer;
     //number = 150 : assign that it is 150.
@@ -39,7 +39,6 @@ int main(){
     std::string desert = "ice cream";
     std::string pharse = "My name is Peter.";
 
-    // std::cout << "I will graduate in " << year << " from NYUST." << '\n';
     // std::cout << "I like " + desert << " because it is delicious." << std::endl;
 
     //constant variable which is read-only
@@ -47,7 +46,7 @@ int main(){
     const double PI = 3.14159;
     const bool NOTFAIR = true;
     const char HIGEST_LEVEL = 'A';
-    const std::string SCHOOL_NAME = "National Yunlin University of Science and Technology";
+    const std::string SCHOOL_NAME = "University";
     std::cout << HIGEST_LEVEL << std::endl;
 
     return 0;
