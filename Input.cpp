d164126283f41b086b38140227ebe354a77e42fa
Peter0803 <111713800+Peter0803@users.noreply.github.com>
2022-10-01 15:35:15 +0800

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -3,28 +3,23 @@
 int main()
 {
     std::string food;
-    std::string country;
     std::string name;
     int number;
 
     //std::cin  get the user input
-    std::cout << "What is your name? ";
-    std::cin >> name;
-    
     //std::getline(std::cin, variable)  the input can contain spaces
-    std::cout << "Which country do you want to go to? ";
-    std::getline(std::cin, country);
+    std::cout << "Enter your full name : ";
+    std::getline(std::cin, name);
 
     std::cout << "How many students in the classroom? ";
     std::cin >> number;
     
     //std::getline(std::cin >> std::ws, variable) the input will not be "\n" when the previous input is integer or double
-    std::cout << "What is your favorite food? ";
+    std::cout << "What is favorite food? ";
     std::getline(std::cin >> std::ws, food);
 
     std::cout << "My name is " << name << "." << std::endl;
     std::cout << "My favorite food is " << food << "." << std::endl;
-    std::cout << "I want to go to " << country << "." << std::endl;
     std::cout << "There are " << number << " students in the classroom." << std::endl;
 
     return 0;
